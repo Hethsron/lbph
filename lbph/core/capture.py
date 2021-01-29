1ce717c72d9f64642735d0b1e560fae1027c1aed
Hethsron <hethsronboueya@gmail.com>
2021-01-29 02:53:33 +0100

    update branch

--- lbph/core/capture.py
+++ lbph/core/capture.py
@@ -109,7 +109,7 @@ class shooting(object):
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                 faces = detector.detectMultiScale(gray, 1.3, 5)
                 for (x,y,w,h) in faces:
-                    cv2.rectangle(img, (x,y), (x + w, y + h), (255,0,0), 2)     
+                    cv2.rectangle(img, (x,y), (x + w, y + h), (255, 0, 0), 2)     
                     counter += 1
 
                     # Save the captured image into the datasets folder
